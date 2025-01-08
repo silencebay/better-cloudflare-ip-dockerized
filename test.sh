26ca4d005035e841c5f5dc1c7459004fc3b73015
silencebay <silencebay@users.noreply.github.com>
2025-01-08 19:15:43 +0800

    test: Increase IP selection consideration
    - Increase the number of candidate IPs to evaluate for optimal selection.

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 work_dir=/usr/local/bin/CloudflareSpeedTest
 cd $work_dir
 ./CloudflareST $ARGS -o /tmp/result.csv
-best_ip=$(cat /tmp/result.csv | grep -v "IP" | head -n 1 | awk -F, '{print $1}')
-echo $best_ip > /data/ip.txt
+best_ip=$(cat /tmp/result.csv | grep -v "IP" | head -n 10 | awk -F, '{print $1}')
+echo "${best_ip}" > /data/ip.txt
 env | grep GIST_ &>1 && gist.sh /data/ip.txt
